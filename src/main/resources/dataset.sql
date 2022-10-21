7e6beaadd39de6775bcac018881bac523ecb8f90
Nicolas Massé <nicolas.masse@itix.fr>
2022-10-21 15:32:14 +0200

    incidents are now stored in database

---
+++ src/main/resources/dataset.sql
@@ -0,0 +1,8 @@
+INSERT IGNORE INTO Incident
+    (
+        id, date, description, status
+    )
+VALUES
+    (1, "2022-10-21", "coupure fibre", 0),
+    (2, "2022-10-22", "panne émetteur", 0),
+    (3, "2022-10-22", "le datacenter a grillé", 0);
