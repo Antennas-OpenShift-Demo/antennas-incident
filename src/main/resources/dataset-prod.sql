1dc759cb21e9b5a0ca819094dd9ea17eeeb9cb4c
Nicolas Massé <nicolas.masse@itix.fr>
2022-11-23 10:22:42 +0100

    postgresql version

--- src/main/resources/dataset-prod.sql
+++ src/main/resources/dataset-prod.sql
@@ -1,11 +1,12 @@
-INSERT IGNORE INTO Incident
+INSERT INTO Incident
     (
         id, date, description, status
     )
 VALUES
-    (1, "2022-10-21", "coupure fibre, paris", 0),
-    (2, "2022-10-20", "coupure fibre, marseille", 1),
-    (3, "2022-10-22", "panne émetteur, brest", 0),
-    (4, "2022-10-23", "panne émetteur, metz", 1),
-    (5, "2022-10-24", "panne émetteur, strasbourg", 1),
-    (6, "2022-10-25", "panne de courant, bourges", 0);
+    (1, '2022-10-21', 'coupure fibre, paris', FALSE),
+    (2, '2022-10-20', 'coupure fibre, marseille', TRUE),
+    (3, '2022-10-22', 'panne émetteur, brest', FALSE),
+    (4, '2022-10-23', 'panne émetteur, metz', TRUE),
+    (5, '2022-10-24', 'panne émetteur, strasbourg', TRUE),
+    (6, '2022-10-25', 'panne de courant, bourges', FALSE)
+ON CONFLICT DO NOTHING;
